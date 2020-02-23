ee6bc8911929f56b8cc4ceb01332206fc7474c9d
Nagger <devpack@mail.ru>
2020-02-23 17:59:20 +0300

    Resolved snail

--- snail/main.js
+++ snail/main.js
@@ -1,3 +1,32 @@
 snail = function(array) {
   // enjoy
-}
+  let arrayResult = [];
+  while (array.length) {
+    array.length && arrayResult.push(...array.shift());
+    array.length &&
+      arrayResult.push(
+        ...array.reduce((acc, item, index) => {
+          acc.push(array[index].pop());
+          return acc;
+        }, [])
+      );
+    array.length && arrayResult.push(...array.pop().reverse());
+    array.length &&
+      arrayResult.push(
+        ...array
+          .reduce((acc, item, index) => {
+            acc.push(array[index].shift());
+            return acc;
+          }, [])
+          .reverse()
+      );
+  }
+  return arrayResult;
+};
+const array = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+
+console.log(snail(array));
