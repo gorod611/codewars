03d1df3febf6d08b182a4564fc67e8cabd6e5ced
Nagger <devpack@mail.ru>
2020-02-28 12:37:28 +0300

    complete postfix converter

--- infix-to-postfix-converter/main.js
+++ infix-to-postfix-converter/main.js
@@ -1,44 +1,42 @@
-function toPostfix (infix) {
+function toPostfix(infix) {
   // Convert infix to postfix here, and return result.
   let out = [];
   let stack = [];
   const ops = {
-    '(':1,
-    ')':1,
-    '+':2,
-    '-':2,
-    '*':3,
-    '/':3,
-    '^':4
+    "(": 1,
+    ")": 1,
+    "+": 2,
+    "-": 2,
+    "*": 3,
+    "/": 3,
+    "^": 4
   };
   const operators = Object.keys(ops);
   for (item of Array.from(infix)) {
     if (item.match(/[0-9]/)) {
-      out.push(item)
-    } else {
-      if (item === '(') {
-        stack.unshift(item)
-      } else {
-        if (operators.includes(item)) {
-          if(item === ')') {
-            const posScob = stack.indexOf('(');
-            out.push(...stack.splice(0, posScob));
-            stack.shift();
-          } else {
-            if(ops[stack[0]] && ops[stack[0]] >= ops[item]) {
-              out.push(stack[0]);
-              stack.shift();
-              stack.unshift(item)
-            } else {
-              stack.unshift(item)
-            }
-          }
-        }
+      out.push(item);
+      continue;
+    }
+    if (item === "(") {
+      stack.unshift(item);
+      continue;
+    }
+    if (item === ")") {
+      const posScob = stack.indexOf("(");
+      out.push(...stack.splice(0, posScob));
+      stack.shift();
+      continue;
+    }
+    if (operators.includes(item)) {
+      while (ops[stack[0]] && ops[stack[0]] >= ops[item]) {
+        out.push(stack[0]);
+        stack.shift();
+      }
+      stack.unshift(item);
+    }
   }
-  }
-  }
-  console.log(out);
-console.log(stack);
+
+  return out.concat(stack).join("");
 }
 
 console.log(toPostfix("5+(6-2)*9+3^(7-1)"));
